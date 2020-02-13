65bb74c789d09705d289926aa726672ebd1a20b9
fstoaldo <fstoaldo@gmail.com>
2020-02-13 07:06:10 -0400

    new class with note methods

--- src/keep_importer.py
+++ src/keep_importer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import json
 import src.note as note
 
 # Owned
@@ -13,92 +12,8 @@ __email__ = "fstoaldo@gmail.com"
     plain-text format, to facilitate backup and make it easier to import to other note taking apps"""
 
 
-def open_json(json_file):
-    """Opens a new json file
-        :param json_file
-        :return data contained in json_file"""
-
-    with open(json_file, encoding="utf-8") as f:
-        return json.load(f)
-
-
-def write_header(file, data):
-    """Writes the header of the note
-        :param file: note file
-        :param data: data source from json file
-        :return file with the header written on it"""
-
-    if "title" in data:
-        if data["title"] != "":
-            file.write(data["title"] + "\n\n")
-
-    return file
-
-
-def write_contents(file, data):
-    """Write contents from data file into new note file
-        :param file: note file
-        :param data: data source from json file
-        :return file with the contents written on it"""
-
-    # handle annotations (rich links)
-    if "annotations" in data:
-        file.write("ANNOTATIONS\n===========\n")
-        for item in data["annotations"]:
-            file.write("TITLE: " + item["title"] + "\n")
-            file.write("SOURCE: " + item["url"] + "\n")
-            file.write("DESCRIPTION: " + item["description"] + "\n--------\n")
-
-    elif "listContent" in data:
-        # checklist
-        for item in data["listContent"]:
-            if item["isChecked"]:
-                file.write("[X]" + item["text"] + "\n")
-            else:
-                file.write("[]" + item["text"] + "\n")
-
-    elif "textContent" in data:
-        file.write(data["textContent"])
-
-    return file
-
-
-def write_footer(file, data):
-    """Write footer  into new note file
-        :param file: note file
-        :param data: data source from json file
-        :return file with the footer written on it"""
-
-    if "labels" in data:
-        file.write("\n\n Tags (from Google Keep):\n\t")
-        for label in data["labels"]:
-            file.write(label["name"] + ",")
-
-    return file
-
-
-def new_note(json_file):
-    """Creates a new plain text note from a json file
-        :param json_file: a google keep json file note"""
-
-    # open json file and get its data
-    data = open_json(json_file)
-    # create a new txt file to paste the notes into only if the required fields are present
-    if "listContent" in data or "textContent" in data:
-        new_file = open(json_file.name.replace(".json", ".txt"), "w+")
-        # write the title of the note as the header if present
-        new_file = write_header(new_file, data)
-        # write the contents of the note
-        new_file = write_contents(new_file, data)
-        # write footer
-        new_file = write_footer(new_file, data)
-    else:
-        print("Could not import file <{}>: 'listContent' or"
-              "'textContent' fields missing".format(json_file.name))
-
-
 def open_json_directory(path):
-    """Opens up a directory containing json files to import
+    """Scans a directory searching for json files to import
         :param path of the directory to open"""
 
     # open the directory and loop through files
@@ -106,7 +21,10 @@ def open_json_directory(path):
         if file.path.endswith(".json") and file.is_file():
             # found a json file, try to import it
             print("IMPORTING: {}".format(file.name))
-            new_note(file)
+            # new Note object
+            n = note.Note(file)
+            # create a new txt file from it
+            n.make_note()
 
 
 def main():
--- src/note.py
+++ src/note.py
@@ -1,9 +1,83 @@
 #!/usr/bin/env python
 
-import os
 import json
 
 # Owned
 __author__ = "fstoaldo"
 __maintainer__ = "fstoaldo"
 __email__ = "fstoaldo@gmail.com"
+
+
+class Note:
+    """Note class to hold google keep json data and
+        with methods to export as plain-text .txt files"""
+
+    def __init__(self, json_file):
+        """Constructs a new Note object"""
+        with open(json_file, encoding="utf-8") as f:
+            self.data = json.load(f)
+
+        self.txt_note = open(json_file.name.replace(".json", ".txt"), "w+")
+
+    def make_note(self):
+        """Creates a new plain text note from a json file"""
+        # write the title of the note as the header if present
+        self._write_header()
+        # write the contents of the note
+        self._write_contents()
+        # write footer
+        self._write_footer()
+
+        return self.txt_note
+
+    """Helper methods to new_note defined below"""
+
+    def _write_header(self):
+        """Writes the header of the note"""
+        if "title" in self.data:
+            if self.data["title"] != "":
+                self.txt_note.write(self.data["title"] + "\n\n")
+
+    def _write_footer(self):
+        """Write footer  into new note file"""
+        if "labels" in self.data:
+            self.txt_note.write("\n\n Tags (from Google Keep):\n\t")
+            for label in self.data["labels"]:
+                self.txt_note.write(label["name"] + ",")
+
+    def _write_contents(self):
+        """Write contents from data file into new note file"""
+        # handle annotations (rich links)
+        if "annotations" in self.data:
+            self._write_annotations()
+
+        elif "listContent" in self.data:
+            self._write_list()
+
+        elif "textContent" in self.data:
+            self._write_text()
+
+        else:
+            self.txt_note.write("<<NO NOTES HAVE BEEN FOUND>>")
+
+    """Helper methods to private method _write contents defined below"""
+
+    def _write_annotations(self):
+        """Handles annotations (rich links)"""
+        self.txt_note.write("ANNOTATIONS\n===========\n")
+        for item in self.data["annotations"]:
+            self.txt_note.write("TITLE: " + item["title"] + "\n")
+            self.txt_note.write("SOURCE: " + item["url"] + "\n")
+            self.txt_note.write("DESCRIPTION: " + item["description"] + "\n--------\n")
+
+    def _write_list(self):
+        """Handles lists"""
+        for item in self.data["listContent"]:
+            if item["isChecked"]:
+                self.txt_note.write("[X]" + item["text"] + "\n")
+            else:
+                self.txt_note.write("[]" + item["text"] + "\n")
+
+    def _write_text(self):
+        """Handles text notes"""
+        self.txt_note.write(self.data["textContent"])
